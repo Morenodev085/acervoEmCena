db49f9c59cb76883e68ac837577aadbb7ebb19fc
moreno <moreno.ferreira11@gmail.com>
2025-07-22 15:27:23 -0300

    .

---
+++ src/Componets/footer/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from "../../styled"
+
+const Footer =() => {
+    return(
+        <Container>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <a href="">@niteroiemcena</a>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default Footer
