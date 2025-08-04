1fba882e8be7643c19ce5ba9867e13b48880247d
Morenodev085 <moreno.ferreira11@gmail.com>
2025-08-04 16:23:53 -0300

    cores botoes

--- src/Componets/Botons/index.tsx
+++ src/Componets/Botons/index.tsx
@@ -7,11 +7,11 @@ const Buttons = () => {
             <ButtonArea>
                 <Button ButtonColor={colors.green} >Mostra Peças</Button>
                 <Button ButtonColor={colors.pink} >Mostra Rua</Button>
-                <Button ButtonColor={colors.brown}  >Mostra Cenas Curtas</Button>
+                <Button ButtonColor={colors.purple}  >Mostra Cenas Curtas</Button>
             </ButtonArea>
             <ButtonArea>
                 <Button ButtonColor={colors.yellow}>Mostra Estudantil</Button>
-                <Button ButtonColor={colors.purple} >Atividades Formativas</Button>
+                <Button ButtonColor={colors.brown} >Atividades Formativas</Button>
                 <Button ButtonColor={colors.cream} >Rodada de Negócios</Button>
             </ButtonArea>
         </ContainerSite>
