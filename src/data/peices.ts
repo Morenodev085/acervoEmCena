65dfb2260c9f1933c9a443cfbb426459ca653f42
moreno <moreno.ferreira11@gmail.com>
2025-08-12 13:08:33 -0300

    pecas longas

--- src/data/peices.ts
+++ src/data/peices.ts
@@ -3,43 +3,43 @@ import type { Piece } from './types';
 export const pieces: Piece[] = [
   {
     id: 1,
-    title: 'Peça Longa muito muito  muito',
+    title: 'Kawó – o rei chama',
     type: 'longa',
-    description: 'Peça mais densa...',
+    description: 'Kawó – o rei chama apresenta para um público uma África ancestral e imaginária e narra a preparação do “dia do Obá Xangô”. Uma família composta por uma mãe, quatro filhos e um avó passam o dia desde seu alvorecer preparando a festividade. Tudo sob o comando dessa grande matriarca, os seis trabalham para que tudo seja perfeito para celebrar a memória do rei, esse ancestral tão admirado por todos eles. Enquanto cozinham e decoram o quintal para festa da fogueira que será no fim do dia, a família relembra as narrativas que compõem a trajetória que levaram o Xangô menino se tornar um dos mais respeitados e cultuados orixás. Histórias de batalhas, amores e relações familiares costuram o dia dessa família e trazem algumas reflexões sobre qual o limite do poder e também de como é possível errar tentando fazer o bem.',
     image: 'https://images.pexels.com/photos/9774919/pexels-photo-9774919.jpeg?_gl=1*1iekv4c*_ga*MTk0OTU4ODQ2My4xNzUzMjcyMzIw*_ga_8JE65Q40S6*czE3NTMyNzIzMTkkbzEkZzEkdDE3NTMyNzM0NjYkajM4JGwwJGgw',
-    data: 18
+    data: 16
   },
   {
     id: 2,
-    title: 'Peça Longa2',
-    type: 'curta',
-    description: 'Peça mais densa...',
+    title: 'Policarpo Quaresma – O Musical',
+    type: 'longa',
+    description: "Adaptado do classico Triste Fim de Policarpo Quaresma, de Lima Barreto, o espetáculo é uma homenagem e celegração do teatro de revista e à nossa literatura.Ao apresentar à      novos públicos um dos grandes persoagens da ficção brasileira, o espetáculo irá, com  dança, música e energia, levar emoção e reflexão para públicos que desconhecem o patriota Policarpo e suas ideias pouco convencionais de como mudar e salvar o Brasil.",
     image: 'https://images.pexels.com/photos/1042436/pexels-photo-1042436.jpeg?_gl=1*12wn9vd*_ga*MTk0OTU4ODQ2My4xNzUzMjcyMzIw*_ga_8JE65Q40S6*czE3NTMyNzIzMTkkbzEkZzEkdDE3NTMyNzM0OTQkajEwJGwwJGgw',
-    data: 18
+    data: 24
   },
   {
     id: 3,
-    title: 'Peça Longa3',
+    title: 'Terapia Infernal',
     type: 'longa',
-    description: 'Peça mais densa...',
+    description: 'Quem verdadeiramente tem a coragem de se expor para si mesmo? Quem consegue habitar essa zona desconfortável do questionamento? Que personagem sustentaria uma análise perante a todos nós? O Diabo escolhe a dedo seus pacientes que farão vir à tona as observações mais relevantes sobre a humanidade atual, com muito humor e musicalidade.',
     image: 'https://images.pexels.com/photos/7055718/pexels-photo-7055718.jpeg?_gl=1*13xppyh*_ga*MTk0OTU4ODQ2My4xNzUzMjcyMzIw*_ga_8JE65Q40S6*czE3NTMyNzIzMTkkbzEkZzEkdDE3NTMyNzM1NzIkajQkbDAkaDA.',
-    data: 18
+    data: 25
   },
   {
     id: 4,
-    title: 'Peça Longa4',
-    type: 'estudantil',
-    description: 'Mostra Estudantil',
-    data: 18
+    title: 'Espetáculo Macacos',
+    type: 'longa',
+    description: 'MACACOS é um espetáculo sobre como o racismo está enraizado na história do Brasil. A peça é uma reflexão sobre o adjetivo MACACO, usado contra pessoas negras em todo o mundo. Além disso, a colonização é abordada para justificar a estrutura social e cotidiana do Brasil de 1500 até os dias de hoje.',
+    data: 26
   },
   {
     id: 5,
-    title: 'Rua',
-    type: 'rua',
-    description: 'Mostra Estudantil',
-    data: 19
+    title: 'Por que não nos?',
+    type: 'longa',
+    description: 'Os atores usam a comédia para discutir tabus do universo dos homens. Quando três amigos, muito próximos, e que sabem tudo da vida uns dos outros se juntam para abrir uma discussão que reflita sobre a construção da masculinidade, não tem como o roteiro dar errado! Assim surgiu a ideia de Por que não nós?.',
+    data: 27
   },
-    {
+  {
     id: 6,
     title: 'Radad',
     type: 'rodada',
