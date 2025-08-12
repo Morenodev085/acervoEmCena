b8320f0e49d05a1298e7df3d8de940abb4d448d5
moreno <moreno.ferreira11@gmail.com>
2025-08-12 16:13:15 -0300

    pecas para lista

--- src/data/peices.ts
+++ src/data/peices.ts
@@ -41,9 +41,135 @@ export const pieces: Piece[] = [
   },
   {
     id: 6,
-    title: 'Radad',
-    type: 'rodada',
-    description: 'Mostra Estudantil',
+    title: 'O Último Ato',
+    type: 'curta',
+    description: 'Em um regime que baniu a arte do povo, os palhaços Cícero e Pulonin mantêm um teatro clandestino. Cercados pelos Atormentadores e sem plateia, encenam um último espetáculo, desafiando o horror com riso e resistência.',
+    data: 19
+  },
+  {
+    id: 7,
+    title: 'CLARO!',
+    type: 'curta',
+    description: 'Beto e Bete se encontram e, a cada toque de campainha, recomeçam a conversa com variações inesperadas. Entre encontros, desencontros e identidades mutantes, a peça revela, com humor e ritmo, como o acaso e o timing moldam nossas escolhas, desejos e possibilidades de conexão.',
+    data: 19
+  },
+  {
+    id: 8,
+    title: 'Sob a pele',
+    type: 'curta',
+    description: 'Não há.',
+    data: 19
+  },
+  {
+    id: 9,
+    title: 'Próxima à fronteira',
+    type: 'curta',
+    description: '“próxima à fronteira” propõe a invenção de uma ancestralidade artística a partir da vida e obra de Ana Mendieta, entrelaçando autobiografia e fabulação. A cena apresenta o comprometimento com os sonhos e práticas de cuidado através de ervas medicinais como forma de escrever com a artista em questão.',
+    data: 19
+  },
+  {
+    id: 10,
+    title: 'Relatos de professores',
+    type: 'curta',
+    description: 'Quatro professores internados em um manicômio são levados à sala de psiquiatria, onde participarão de uma sessão de análise investigativa. Durante essa sessão, serão tiradas conclusões sobre os eventos narrados, gerando no público uma tensão entre o cômico e o dramático.',
+    data: 19
+  },
+  {
+    id: 11,
+    title: 'Ele Veio',
+    type: 'curta',
+    description: 'Ele Veio é a cena que revisita Jesus como homem: real, humano e atravessado por um abandono do estado. Um Cristo perseguido, torturado e silenciado. Entre tecnologias, músicas, poesias, vídeos e luz, a cena resgata a força viva de um revolucionário que veio e continua vindo,nos rostos silenciados do mundo.',
     data: 20
+  },
+  {
+    id: 12,
+    title: 'Facada',
+    type: 'curta',
+    description: '"Facada" é uma história real. Através de uma paródia à "auto ficção", a cena relata a tentativa de assassinato que o ator Mau Moreira sofreu. Os personagens que permeiam o incidente aparecem de forma caricata e "bufônica", dentro de uma perspectiva confusa do que pode ser uma experiência de quase morte.',
+    data: 20
+  },
+  {
+    id: 13,
+    title: 'Teresas',
+    type: 'curta',
+    description: 'Em um regime que baniu a arte do povo, os palhaços Cícero e Pulonin mantêm um teatro clandestino. Cercados pelos Atormentadores e sem plateia, encenam um último espetáculo, desafiando o horror com riso e resistência.',
+    data: 20
+  },
+  {
+    id: 14,
+    title: 'Quarto 105',
+    type: 'curta',
+    description: 'Em um prostíbulo no Rio, Bertha, ex-rainha da casa, enfraquecida e desiludida, enfrenta a dona do local. Um segredo perigoso vem à tona. Enquanto isso, uma prostituta recém chegada pode virar o jogo. Em meio a disputas de poder e sobrevivência, o direito de existir e permanecer é ameaçado.',
+    data: 20
+  },
+  {
+    id: 15,
+    title: 'Espelho, Voz e Raíz',
+    type: 'curta',
+    description: 'A partir da metáfora do espelho como memória, ferida e cura, Espelho, Voz e Raiz é um mergulho cênico e ritualístico na ancestralidade de mulheres negras e nas cicatrizes deixadas pelo patriarcado. Seis vozes femininas, marcadas por diferentes trajetórias e temperamentos, revelam as múltiplas formas de dor, silenciamento e resistência que atravessam seus corpos e histórias. Ao revisitar suas raízes, elas desafiam estigmas, quebram moldes e transformam feridas em verbo. O público não assiste de fora: é convocado como espelho, cúmplice e parte da transformação.',
+    data: 20
+  },
+  {
+    id: 16,
+    title: 'José Não é Ninguém',
+    type: 'curta',
+    description: 'Zé, um pré-adolescente que se vê confuso e curioso ao explorar sua identidade LGBTQIAPN+, cresce em um lar desestruturado e luta para entender como ser aceito na sociedade nos anos 2000 e cada descoberta traz consigo um sentimento: o encanto do primeiro amor, o peso do bullying que o persegue como uma sombra e a dor do luto. Em meio a tantos desafios e questionamentos ele busca respostas em um mundo que muitas vezes parece não ter lugar para ele.',
+    data: 21
+  },
+  {
+    id: 17,
+    title: 'Aniversário Guanabara',
+    type: 'curta',
+    description: 'Ao imaginar um futuro quase distópico devastado pelo aquecimento global, o espetáculo ambienta-se em um Rio de Janeiro marcado pela escassez de alimentos e por uma crise sem precedentes. Diante da inevitabilidade de fechar as portas, a rede de supermercados Guanabara opta por um último gesto e propõe a realização do último Aniversário Guanabara. Tal evento, antes tão comum, agora, sob estas circunstâncias, revela as atuais desigualdades que ainda permeiam o estado.',
+    data: 21
+  },
+  {
+    id: 18,
+    title: 'Guinnârou, o rei dos guinnés',
+    type: 'curta',
+    description: 'Em algum lugar da África, um agricultor chamado Sabouyouma resolve montar seu campo no território de uma criatura fantástica, chamada Guinnârou. Movido pela ambição, o homem desconsidera os avisos dos moradores do seu vilarejo sobre o perigo iminente.',
+    data: 21
+  },
+  {
+    id: 19,
+    title: 'Cura',
+    type: 'curta',
+    description: 'Em um Brasil movido pela ganância, o influencer Hugo mergulha no mundo das criptomoedas e do crime. Entre poder, manipulação e traição, ele enfrenta uma crise que revela seus limites. "CURA" é a vida de um homem sedento por sucesso.',
+    data: 21
+  },
+  {
+    id: 20,
+    title: 'Entretempo',
+    type: 'curta',
+    description: 'Com canções originais, Entretempo acompanha Beto e Elisa revivendo, entre memórias embaralhadas, um amor marcado pelo acaso e pelo tempo. A cena transita entre passado e presente para refletir sobre encontros, transformações e o que permanece na lembrança de uma história.',
+    data: 21
+  },
+    {
+    id: 21,
+    title: 'CHACCO',
+    type: 'rua',
+    description: '',
+    data: 20
+  },
+  {
+    id: 22,
+    title: 'Inka Clown Show',
+    type: 'rua',
+    description: 'Inka Clown é um palhaçõ latinoamericano, e vem das mais altas terras da américa para mostrar o que traz na sua maleta: uma porção de surpresas que garantem o riso e a diversão. Tudo acompanhado de acrobacias, equilibrismo, truques e muita música. Inka clown show é um espetáculo envolvente e repleto de ternura, graça e beleza. Ao mesmo tempo é cômica e delicada, uma obra poética, que traz em sua essência a expressão cultural do teatro de rua latino.',
+    data: 21
+  },
+  {
+    id: 22,
+    title: 'Como Cozinhar uma Criança',
+    type: 'rua',
+    description: 'Em um programa de culinária ao vivo, dois cozinheiros ensinam uma receita cujos ingredientes principais são, nada mais e nada menos, que... CRIANÇAS. A ideia é preparar devidamente os pequenos para que não se tornem adultos intragáveis. O problema aparece quando os chefs não concordam quanto aos métodos de preparo. E agora? Quem será que vai acabar indo pra panela?',
+    data: 26
+  },
+    {
+    id: 23,
+    title: 'O Abrazo Girafa',
+    type: 'rua',
+    description: 'O Abrazo Girafa é um espetáculo solo, de rua e para toda família. Malabarismo com piões acrobáticos acompanham o grande desafio que dá nome ao espetáculo. O clássico número de equilibrismo com um monociclo girafa, que neste caso mede mais de 2 metros. Subir nas alturas , equilibrar uma bola de cristal no topo da cabeça e malabarear com três facões é o desafio. Acreditar no impossível se torna necessário para abraçar a sua criança interior.',
+    data: 28
   },
 ]
