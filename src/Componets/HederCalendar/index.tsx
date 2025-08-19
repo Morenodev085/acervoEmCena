a4152ce1578b5a5103d7906411b624133573c250
moreno <moreno.ferreira11@gmail.com>
2025-08-19 17:35:21 -0300

    .

--- src/Componets/HederCalendar/index.tsx
+++ src/Componets/HederCalendar/index.tsx
@@ -8,8 +8,8 @@ const HerderCalendar = () => {
 
     <DivHerderCalendario>
         <TituloCalendario>Programação Completa</TituloCalendario>
-        <TextoCalendario>Venha conferir toda a programação do Niterói em Cena – Festival Internacional de Teatro 2025.
-            Todas as atrações são gratuitas!</TextoCalendario>
+        <TextoCalendario>Venha conferir toda a programação do Niterói em Cena – Festival Internacional de Teatro 2025. Todas as atrações são gratuitas!
+<br/>Colabore com 1 kg de alimento não perecível.</TextoCalendario>
     </DivHerderCalendario>
         </ContainerSite>
 )}
