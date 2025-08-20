c5f2b7bdcb7d3da357392fcd9847bc526c128f0f
moreno <moreno.ferreira11@gmail.com>
2025-08-20 20:26:55 -0300

    .

--- src/Componets/descricaoFestival/intex.tsx
+++ src/Componets/descricaoFestival/intex.tsx
@@ -11,7 +11,9 @@ const DescricaoSimplesComImagem = () => {
                     <TituloSimples>Maioridade Artística <br />18 anos de existência</TituloSimples>
                     <p>
                         Em 2025, completamos dezoito edições consecutivas. Ao longo desse período, o Niterói em Cena trouxe para a cidade grandes nomes do teatro nacional e diversas atrações internacionais.
-                        Nesta edição, temos a honra de receber artistas de mais de seis estados brasileiros (Alagoas, Bahia, Minas Gerais, Pernambuco, Rio de Janeiro e São Paulo), além de representantes de três países da América Latina (Argentina, Chile e Peru).
+                        Nesta edição, temos a honra de receber artistas de seis estados brasileiros 
+                        (Alagoas, Bahia, Minas Gerais, Pernambuco, Rio de Janeiro e São Paulo), além de representantes de três países da 
+                        América Latina (Argentina, Chile e Peru).
                         Vamos oferecer quatro Atividades Formativas e uma Rodada de Negócios, que conectará artistas e produtores a gestores culturais.
                     </p>
                 </TextSimples>
