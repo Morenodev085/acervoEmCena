26cb1e31cdb0300774000050dd57f67807f54ee0
moreno <moreno.ferreira11@gmail.com>
2025-08-20 21:31:01 -0300

    .

--- src/Componets/SectionFesta/index.tsx
+++ src/Componets/SectionFesta/index.tsx
@@ -1,9 +1,19 @@
 import { ContainerSite } from "../../styled"
+import encerramento from "../../assets/encerramentoFestival.jpeg"
+import { ContainerEncerramento, ImageEncerramento } from "./styled"
 
-const SectionFestaYaba =() => {
-    return(
+const SectionFestaYaba = () => {
+    return (
         <ContainerSite>
-        
+            <ContainerEncerramento>
+                <ImageEncerramento>
+                    <img src={encerramento} alt="" />
+                </ImageEncerramento>
+                <div>
+                    <h4>festa de encerramento</h4>
+                    <p>nononononononononononononononononononononononononononononono</p>
+                </div>
+            </ContainerEncerramento>
         </ContainerSite>
     )
 }
---
+++ src/Componets/SectionFesta/styled.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const ContainerEncerramento = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+gap: 2rem;
+flex-wrap: wrap;
+margin-top: 24px;
+
+img{
+        max-width: 100%;
+    height: auto;
+    border-radius: 8px;
+}
+`
+export const ImageEncerramento = styled.div`
+  flex: 1;
+  text-align: center;
+  border: 8px;
+
+
+  img {
+    max-width: 100%;
+    height: auto;
+    border-radius: 8px;
+  }
+`
