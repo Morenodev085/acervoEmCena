cf48ef5e4c3ac3d1d1c9f8f0e2ff56d9d7398ae3
moreno <moreno.ferreira11@gmail.com>
2025-08-21 17:42:01 -0300

    .

--- src/Componets/SectionEco/index.tsx
+++ src/Componets/SectionEco/index.tsx
@@ -1,15 +1,12 @@
 import { ContainerSite } from "../../styled"
-import { ContainerEco, ImageEco, TextSimples, TituloSimples } from "./styled"
+import { ContainerEco, , TextSimples, TituloSimples } from "./styled"
 
 const SectionEco = () => {
     return (
         <ContainerSite>
             <ContainerEco>
-                <ImageEco>
-                    <img src="https://cdn.pixabay.com/photo/2020/05/29/18/28/log-5236152_1280.jpg" alt="" />
-                </ImageEco>
                 <TextSimples>
-                    <TituloSimples>Proposta ecologica</TituloSimples>
+                    <TituloSimples>Ações sustentáveis</TituloSimples>
                     <p>O Niterói em Cena – Festival Internacional de Teatro – está alinhado com os Objetivos de Desenvolvimento Sustentável (ODS) da ONU, promovendo diversidade, educação e sustentabilidade. Ao democratizar o acesso à cultura (ODS 4), o festival oferece produções teatrais que dialogam com diferentes públicos. Valorizando a cultura local e fortalecendo a identidade comunitária (ODS 11), contribui para cidades mais inclusivas e resilientes. O evento também adota práticas ambientais responsáveis (ODS 13), como reciclagem e incentivo ao transporte sustentável, oferecendo ingressos para quem chega de bicicleta. O evento é acessível ao possibilitar tradução  em Libras e Audiodescrição em suas sessões.<br /></p>
                 </TextSimples>
             </ContainerEco>
@@ -20,7 +17,3 @@ const SectionEco = () => {
 export default SectionEco
 
 
-
-//    <ImageEco>
-//         <img src="https://cdn.pixabay.com/photo/2020/05/29/18/28/log-5236152_1280.jpg" alt="" />
-//         </ImageEco> 
