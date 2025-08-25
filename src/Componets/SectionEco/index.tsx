d4a9ae11bc85996127a26a7fe41acf3f0fb5cf20
moreno <moreno.ferreira11@gmail.com>
2025-08-24 21:55:40 -0300

    .

--- src/Componets/SectionEco/index.tsx
+++ src/Componets/SectionEco/index.tsx
@@ -6,7 +6,7 @@ const SectionEco = () => {
         <ContainerSite>
             <ContainerEco>
                 <TextSimples>
-                    <TituloSimples>Ações sustentáveis</TituloSimples>
+                    <TituloSimples>Ações Sustentáveis</TituloSimples>
                     <p>O Niterói em Cena – Festival Internacional de Teatro – está alinhado com os Objetivos de Desenvolvimento Sustentável (ODS) da ONU, promovendo diversidade, educação e sustentabilidade. Ao democratizar o acesso à cultura (ODS 4), o festival oferece produções teatrais que dialogam com diferentes públicos. Valorizando a cultura local e fortalecendo a identidade comunitária (ODS 11), contribui para cidades mais inclusivas e resilientes. O evento também adota práticas ambientais responsáveis (ODS 13), como reciclagem e incentivo ao transporte sustentável, oferecendo ingressos para quem chega de bicicleta. O evento é acessível ao possibilitar tradução  em Libras e Audiodescrição em suas sessões.<br /></p>
                 </TextSimples>
             </ContainerEco>
