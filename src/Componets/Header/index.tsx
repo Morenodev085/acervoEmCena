95a38995033ce63d47ceab60e25f3f7d4312ad81
Moreno Almeida <moreno.ferreira11@gmail.com>
2025-09-01 18:11:48 -0300

    volta para padrao

--- src/Componets/Header/index.tsx
+++ src/Componets/Header/index.tsx
@@ -1,5 +1,5 @@
 import trama from '../../assets/tramaNiteroiEmCena.jpeg';
-import logo from '../../assets/logoSite.jpeg'
+import logo from '../../assets/logoSite.png'
 
 import {Logo} from './styled'
 import { ContainerSite } from '../../styled';
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <ContainerSite
-            className="fixed top-0 left-0 right-0 z-50 flex justify-center h-22 bg-cover "
+            className="fixed top-4 left-0 right-0 z-50 flex justify-center h-20 bg-cover "
             style={{ backgroundImage: `url(${trama})`, borderRadius: 12 }}>
             <div className="px-6 py-2 rounded-md shadow-md">
                 <span className="text-xl font-bold select-none">
