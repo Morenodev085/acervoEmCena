1c1503bf234ea14722a241f3cafb4ce3c05d4255
Moreno Almeida <moreno.ferreira11@gmail.com>
2025-09-15 18:07:54 -0300

    .

--- src/data/presenca.ts
+++ src/data/presenca.ts
@@ -13,119 +13,125 @@ import Yonara from '../assets/perfil/Yonara Costa - Conselho de cultura do estad
 import LeonardoS from '../assets/perfil/Leandro Santana - Queimados Encena.png'
 import Rodolfo from '../assets/perfil/Rodolfo Medeiros - FEESPE - Festival de Esquetes e Espetáculos de Petrópolis  .jpeg'
 import Luciana from '../assets/perfil/LUCIANA EZARANI - MOSTRA ASLUCIANAS.jpeg'
-import Mariana from '../assets/perfil/Marilda Ormy - Diretora do Theatro Municipal de Niterói jpg.jpg'
+// import Mariana from '../assets/perfil/'
 import Anila from '../assets/perfil/Anala.jpeg'
 import Luiza from '../assets/perfil/luiza.jpeg'
 import Victor from '../assets/perfil/victor.jpeg'
 import Vini from '../assets/perfil/vini.jpeg'
 import Vinicus from '../assets/perfil/Vinicius.jpeg'
-// import  from '../assets/perfil/'
+import Patrick from '../assets/perfil/Patrick.jpg'
 // import  from '../assets/perfil/'
 
 
 export const presencas: Presenca[] = [
-{
-    nome: "Dárdana Rangel",
-    descricao:"Teatro Popular Oscar Niemeyer",
-    img:Dardana,
-},
-{
-    nome: "Marilda Ormy",
-    descricao:"Diretora do Theatro Municipal de Niterói",
-    img: Marilda ,
-},
-{
-    nome: "Leonardo Carnevale",
-    descricao:"CBTIJ_ASSITEJ Brasil",
-    img: Leonardo,
-},
-{
-    nome: "Cynthia Margareth",
-    descricao:`Aflorar Culltura e FIMC 
+    {
+        nome: "Dárdana Rangel",
+        descricao: "Teatro Popular Oscar Niemeyer",
+        img: Dardana,
+    },
+    {
+        nome: "Marilda Ormy",
+        descricao: "Diretora do Theatro Municipal de Niterói",
+        img: Marilda,
+    },
+    {
+        nome: "Patrick Pessoa",
+        descricao: `Festival de Curitiba e 
+        Festival Midrash`,
+        img: Patrick,
+    },
+    {
+        nome: "Leonardo Carnevale",
+        descricao: "CBTIJ_ASSITEJ Brasil",
+        img: Leonardo,
+    },
+    {
+        nome: "Cynthia Margareth",
+        descricao: `Aflorar Culltura e FIMC 
     Festival Internacional de Máscara do Cariri`,
-    img: Cynthia,
-},
-{
-    nome: "João Carlos Rebelo",
-    descricao:"FITA - Festa Internacional de Teatro",
-    img: JoaoCarlos,
-},
-{
-    nome: "Jeff Fagundes",
-    descricao:"International Theatre Institute Unesco",
-    img: Jeff,
-},
-{
-    nome: "Pablo Rodrigues",
-    descricao:`Gestor do teatro Candido Mendes
+        img: Cynthia,
+    },
+    {
+        nome: "João Carlos Rebelo",
+        descricao: "FITA - Festa Internacional de Teatro",
+        img: JoaoCarlos,
+    },
+    {
+        nome: "Jeff Fagundes",
+        descricao: "International Theatre Institute Unesco",
+        img: Jeff,
+    },
+    {
+        nome: "Pablo Rodrigues",
+        descricao: `Gestor do teatro Candido Mendes
     Gestor da Areninha Sandea de Sá
     Gestor da Areninha Carlos Zéfiro`,
-    img: Pablo,
-},
-{
-    nome: "Ribamar Ribeiro",
-    descricao:"Fataerj e Ciclomáticos",
-    img: Ribamar,
-},
-{
-    nome: "Beatriz Brandão",
-    descricao:"Comitê de Cultura RJ (MinC)",
-    img: Beatriz,
-},
-{
-    nome: "Yonara Costa",
-    descricao:`Conselho de Cultura do Estado
+        img: Pablo,
+    },
+    {
+        nome: "Ribamar Ribeiro",
+        descricao: "Fataerj e Ciclomáticos",
+        img: Ribamar,
+    },
+    {
+        nome: "Beatriz Brandão",
+        descricao: "Comitê de Cultura RJ (MinC)",
+        img: Beatriz,
+    },
+    {
+        nome: "Yonara Costa",
+        descricao: `Conselho de Cultura do Estado
     Representante do Leste Fluminense`,
-    img: Yonara,
-},
-{
-    nome: "Leandro Santana",
-    descricao:"Queimado Encena",
-    img: LeonardoS,
-},
-{
-    nome: "Rodolfo Medeiros",
-    descricao:`Festival de Esquetes e
+        img: Yonara,
+    },
+    {
+        nome: "Leandro Santana",
+        descricao: "Queimado Encena",
+        img: LeonardoS,
+    },
+    {
+        nome: "Rodolfo Medeiros",
+        descricao: `Festival de Esquetes e
     spectáculos de Petrópolis`,
-    img: Rodolfo,
-},
-{
-    nome: "Luciana Ezarani",
-    descricao:"Mostra ASLUCIANAS",
-    img: Luciana,
-},
-{
-    nome: "Mariana Jaco",
-    descricao:"Circuito Carioca de Arte de Rua",
-    img: Mariana,
-},
-{
-    nome: "Anilia Francisca",
-    descricao:`Festar Araruama 
+        img: Rodolfo,
+    },
+    {
+        nome: "Luciana Ezarani",
+        descricao: "Mostra ASLUCIANAS",
+        img: Luciana,
+    },
+    // {
+    //     nome: "Mariana Jaco",
+    //     descricao:"Circuito Carioca de Arte de Rua",
+    //     img: Mariana,
+    // },
+    {
+        nome: "Anilia Francisca",
+        descricao: `Festar Araruama 
     Comitê de Cultura da região dos Lagos` ,
-    img: Anila,
-},
-{
-    nome: "Luiza Iulianello",
-    descricao:`MoviRio Festival  Festival de Dança do Rio de Janeiro.`,
-    img: Luiza,
-},
-{
-    nome: "Vinicius Daumas",
-    descricao:"Festival Internacional de Circo",
-    img: Vinicus ,
-},
-{
-    nome: "Victor Ciattei",
-    descricao:`FENDANIT
+        img: Anila,
+    },
+    {
+        nome: "Luiza Iulianello",
+        descricao: `MoviRio Festival  Festival de Dança do Rio de Janeiro.`,
+        img: Luiza,
+    },
+    {
+        nome: "Vinicius Daumas",
+        descricao: "Festival Internacional de Circo",
+        img: Vinicus,
+    },
+    {
+        nome: "Victor Ciattei",
+        descricao: `FENDANIT
 FESTIVAL NACIONAL DE DANÇA DE NITERÓI`,
-    img: Victor ,
-},
-{
-    nome: "Vini Cristóvão",
-    descricao:"Tiradentes em Cena",
-    img: Vini ,
-},
+        img: Victor,
+    },
+    {
+        nome: "Vini Cristóvão",
+        descricao: "Tiradentes em Cena",
+        img: Vini,
+    },
 ]
 
 
